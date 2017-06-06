62b52b690b765b0977f919b63426d46d528caf3a
sumitkarna <sumit.karna@in.ibm.com>
2017-06-06 13:15:32 +0530

    Add files via upload

--- routes/dbservice.js
+++ routes/dbservice.js
@@ -1,9 +1,36 @@
 
 var mongoose = require('mongoose');
-   var db;
-if (process.env.VCAP_SERVICES) {
-   var env = JSON.parse(process.env.VCAP_SERVICES);
-   db = mongoose.createConnection(env['mongodb-2.2'][0].credentials.url);
+var db;
+var cfenv = require('cfenv');
+var appenv = cfenv.getAppEnv();
+var MongoClient = require("mongodb").MongoClient;
+var services = appEnv.services;
+
+if (process.env.VCAP_SERVICES) { 
+
+var mongodb_services = services["compose-for-mongodb"];
+
+var credentials = mongodb_services[0].credentials;
+
+var ca = [new Buffer(credentials.ca_certificate_base64, 'base64')];
+
+MongoClient.connect(credentials.uri, {
+        mongos: {
+            ssl: true,
+            sslValidate: true,
+            sslCA: ca,
+            poolSize: 1,
+            reconnectTries: 1
+        }
+    },
+    function(err, db) {
+        if (err) {
+            console.log(err);
+        } else {
+            db = db.db("events");
+        }
+    }
+);
 } else {
    db = mongoose.createConnection('localhost', 'ikeasocialapp');
 }
