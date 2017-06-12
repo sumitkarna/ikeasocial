6459ac752effad41bd4e5d2a932c232c006fde44
Sumit Karna <sumit.karna@in.ibm.com>
2017-06-12 08:01:07 +0000

    db db db

--- routes/dbservice.js
+++ routes/dbservice.js
@@ -4,27 +4,32 @@ var db;
 var cfenv = require('cfenv');
 var appenv = cfenv.getAppEnv();
 
-db = mongoose.connect('mongodb://sumit:Passw0rd@ds129610.mlab.com:29610/ikeasocial');
+if (process.env.VCAP_SERVICES) {
 
-//if (process.env.VCAP_SERVICES) { 
-//
-//var mongoDbUrl, mongoDbOptions = {};
-//var mongoDbCredentials = appenv.getServiceCreds("compose-for-mongodb").credentials;
-//var ca = [new Buffer(mongoDbCredentials.ca_certificate_base64, 'base64')];
-//mongoDbUrl = mongoDbCredentials.uri;
-//mongoDbOptions = {
-//mongos: {
-//  ssl: true,
-//    sslValidate: true,
-//    sslCA: ca,
-//    poolSize: 1,
-//    reconnectTries: 1
-//  }
-//};
-//console.log("Connecting to", mongoDbUrl);
+var services = appenv.services;
+
+var mongodb_services = services["compose-for-mongodb"];
+
+var mongoDbCredentials = mongodb_services[0].credentials;
+
+var mongoDbUrl, mongoDbOptions = {};
+
+var ca = [new Buffer(mongoDbCredentials.ca_certificate_base64, 'base64')];
+mongoDbUrl = mongoDbCredentials.uri;
+mongoDbOptions = {
+mongos: {
+  ssl: true,
+    sslValidate: true,
+    sslCA: ca,
+    poolSize: 1,
+    reconnectTries: 1
+  }
+};
+console.log("Connecting to", mongoDbUrl);
+db= mongoose.connect(mongoDbUrl, mongoDbOptions);
 //db = mongoose.connect('mongodb://sumit:Passw0rd@ds129610.mlab.com:29610/ikeasocial');
-//} else {
-//   db = mongoose.createConnection('localhost', 'ikeasocialapp');
-//}
+} else {
+   db = mongoose.createConnection('localhost', 'ikeasocialapp');
+}
 
 exports.db = db;
